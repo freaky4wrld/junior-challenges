1d682929b9604905d1d76423efdc975c66ae0865
freaky4wrld <freaky4wrld@gmail.com>
2023-09-06 18:32:16 +0530

    asyncly added the data to be fetch from json file

--- time-tracking-dashboard-main/script.js
+++ time-tracking-dashboard-main/script.js
@@ -1,22 +1,62 @@
+const dailyBtn = document.getElementById('daily');
 const weekBtn = document.getElementById('weekly');
+const monthBtn = document.getElementById('monthly');
+const cardGroup = document.getElementById("card-group");
 const titles = document.querySelectorAll('.title-text');
 const currentStats = document.querySelectorAll('.current-stats');
 const pastStat = document.querySelectorAll('.prev');
 
-weekBtn.addEventListener('click',showWeekData)
+cardGroup.style.visibility = 'hidden';
+weekBtn.addEventListener('click',showWeekData);
+monthBtn.addEventListener('click',showMonthData);
+dailyBtn.addEventListener('click',showDailyData);
 
-function showWeekData(){
+function showDailyData(){
+    cardGroup.style.visibility = 'visible'
     const xhr = new XMLHttpRequest();
     xhr.open("GET", "data.json", true);
     xhr.onload = function(){
         if(this.status == 200){
             changeTitle(JSON.parse(this.responseText));
             changeCurrentStat(JSON.parse(this.responseText));
+            changePastStat(JSON.parse(this.responseText))
         }
     }
 
     xhr.send();
 }
+
+
+function showWeekData(){
+    cardGroup.style.visibility = 'visible'
+    const xhr = new XMLHttpRequest();
+    xhr.open("GET", "data.json", true);
+    xhr.onload = function(){
+        if(this.status == 200){
+            changeTitle(JSON.parse(this.responseText));
+            changeCurrentWeekStat(JSON.parse(this.responseText));
+            changePastWeekStat(JSON.parse(this.responseText))
+        }
+    }
+
+    xhr.send();
+}
+
+function showMonthData(){
+    cardGroup.style.visibility = 'visible'
+    const xhr = new XMLHttpRequest();
+    xhr.open("GET", "data.json", true);
+    xhr.onload = function(){
+        if(this.status == 200){
+            changeTitle(JSON.parse(this.responseText));
+            changeCurrentMonthStat(JSON.parse(this.responseText));
+            changePastMonthStat(JSON.parse(this.responseText))
+        }
+    }
+
+    xhr.send();
+}
+
 
 function changeTitle(usersTitle){
     let index = 0
@@ -29,7 +69,46 @@ function changeTitle(usersTitle){
 function changeCurrentStat(usersCurrent){
     let index = 0
             currentStats.forEach((stat)=>{
-                stat.innerText = `${usersCurrent[index].timeframes.weekly.current}`;
+                stat.innerText = `${usersCurrent[index].timeframes.daily.current}hrs`;
+                index++;
+            })
+}
+
+function changeCurrentWeekStat(usersCurrent){
+    let index = 0
+            currentStats.forEach((stat)=>{
+                stat.innerText = `${usersCurrent[index].timeframes.weekly.current}hrs`;
+                index++;
+            })
+}
+
+function changeCurrentMonthStat(usersCurrent){
+    let index = 0
+            currentStats.forEach((stat)=>{
+                stat.innerText = `${usersCurrent[index].timeframes.monthly.current}hrs`;
+                index++;
+            })
+}
+function changePastStat(usersCurrent){
+    let index = 0
+            pastStat.forEach((stat)=>{
+                stat.innerText = `${usersCurrent[index].timeframes.daily.previous}hrs`;
+                index++;
+            })
+}
+
+function changePastWeekStat(usersCurrent){
+    let index = 0
+            pastStat.forEach((stat)=>{
+                stat.innerText = `${usersCurrent[index].timeframes.weekly.previous}hrs`;
+                index++;
+            })
+}
+
+function changePastMonthStat(usersCurrent){
+    let index = 0
+            pastStat.forEach((stat)=>{
+                stat.innerText = `${usersCurrent[index].timeframes.monthly.previous}hrs`;
                 index++;
             })
 }
