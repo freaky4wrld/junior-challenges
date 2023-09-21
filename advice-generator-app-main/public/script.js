71689d77bde522dd2a513ac3e627db75e223424b
freaky4wrld <freaky4wrld@gmail.com>
2023-09-21 13:30:00 +0530

    added animation and the fetching function is working good in chrome

--- advice-generator-app-main/public/script.js
+++ advice-generator-app-main/public/script.js
@@ -1,14 +1,20 @@
 const requestBtn = document.getElementById('make-request');
 const idContainer = document.getElementById('advice-id');
 const bodyContainer = document.getElementById('advice-body');
-
+const mainCard = document.querySelector('main');
 const generateAdvice = ()=>{
+    mainCard.classList.add('animate__animated', 'animate__backInDown');
+    requestBtn.classList.add('animate__animated', 'animate__tada', 'animate__delay-1s')
     fetch('https://api.adviceslip.com/advice')
     .then((res)=>res.json())
     .then((data)=>{
         idContainer.innerText = ` # ${data.slip.id}`;
         bodyContainer.innerText = `"${data.slip.advice}"`;
     })
+    setTimeout(()=>{
+        mainCard.classList.remove('animate__animated', 'animate__backInDown');
+        requestBtn.classList.remove('animate__animated', 'animate__tada', 'animate__delay-1s')
+    }, 2500)
 }
 
 requestBtn.addEventListener('click', generateAdvice)
